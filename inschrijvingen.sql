12f2b7d2c445a6f34ad75aa697e9e6eb1b2de4d5
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2019-05-13 15:56:48 +0200

    Update inschrijvingen.sql

---
+++ inschrijvingen.sql
@@ -0,0 +1,23 @@
+INSERT INTO klassen (klas_code, slb_code, cohort) VALUES
+	('lo9e-amo1','crk01', 2019),
+    ('lo9e-amo2','crk02', 2019),
+    ('lo9e-amo3','crk03', 2019);
+    
+INSERT INTO studenten (ov_nummer, geboortedatum, postcode, gewicht, lengte) VALUES
+	(92514,'2002-02-25', '4578 BN', 80, 1.79),
+    (95987,'1997-04-02', '2496 FN', 74, 1.86),
+    (91458,'1999-09-01', '2475 LN', 66, 1.65),
+    (99735,'1995-05-20', '1985 RN', 59, 1.71);
+    
+INSERT INTO klassen_studenten (ov_nummer, klas_code) VALUES
+	(92514,'lo9e-amo1'),
+    (95987,'lo9e-amo1'),
+    (91458,'lo9e-amo2'),
+    (99735,'lo9e-amo3');
+    
+UPDATE studenten
+SET
+	geboortedatum = '2001-02-25'
+WHERE
+	ov_nummer = 92514;
+	
